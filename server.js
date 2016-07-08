3db5cff4456dfcfa70c897e0c31c5731f91de149
Andrew Nichols <annich@microsoft.com>
2016-07-08 13:05:40 -0700

    reorged server.js, removed Addin and SocialMemory, replaced with VstsRest and updated index.html to use the new reference

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
+var bodyParser = require('body-parser');
 var config = require('./config/webpack.prod');
 var authenticate = require('./routes/authenticate');
-var bodyParser = require('body-parser');
-var DEBUG = require('debug');
+var rest = require('./routes/VstsRest');
+var DEBUG = require('./debug');
 
 var app = express();
 var compiler = webpack(config);
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.json());         // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
@@ -21,20 +22,22 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
+
 // Routers
 
-
+app.use('/authenticate', authenticate);
+app.use('/rest', rest);
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/static', express.static(__dirname + '/static'));
-app.use('/authenticate', authenticate);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 if(DEBUG == true){
+  var https = require('https');
   var fs = require('fs');
-  
+
   const options = {
     key: fs.readFileSync('secrets/key.pem'),
     cert: fs.readFileSync('secrets/cert.pem')
